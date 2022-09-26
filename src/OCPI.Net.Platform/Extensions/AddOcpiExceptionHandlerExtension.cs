e75b305565a09966c0c0f87fac0dea3e6d427928
Yuriy Durov <u.a.durov@gmail.com>
2022-09-26 16:41:50 +0400

    Implement OcpiExceptionHandler

---
+++ src/OCPI.Net.Platform/Extensions/AddOcpiExceptionHandlerExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OCPI.Exceptions;
+
+namespace OCPI.Extensions;
+
+public static class AddOcpiExceptionHandlerExtension
+{
+    public static IServiceCollection AddOcpiExceptionHandler(this IServiceCollection services)
+    {
+        return services
+            .AddHttpContextAccessor()
+            .AddScoped<OcpiExceptionHandler>();
+    }
+}
