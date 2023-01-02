d1ad5722c80e15157a5a63e7492c3efe6b56d9d9
Yuriy Durov <u.a.durov@gmail.com>
2023-01-02 17:22:01 +0400

    Make DI Extensions work with just IServiceCollection (#4)

--- src/OCPI.Net/AddOcpiExtension.cs
+++ src/OCPI.Net/AddOcpiExtension.cs
@@ -1,15 +1,22 @@
-﻿#pragma warning disable CA1050 // Declare types in namespaces
-using Microsoft.AspNetCore.Builder;
-using OCPI;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using OCPI.Exceptions;
+using OCPI.Versioning;
+using System.Diagnostics.CodeAnalysis;
 
+[SuppressMessage("Design", "CA1050:Declare types in namespaces")]
 public static class AddOcpiExtension
-#pragma warning restore CA1050 // Declare types in namespaces
 {
     public static WebApplicationBuilder AddOcpi(this WebApplicationBuilder builder)
     {
-        builder.AddOcpiExceptionHandler();
-        builder.AddOcpiVersioning();
-
+        builder.Services.AddOcpi();
         return builder;
+    }
+
+    public static IServiceCollection AddOcpi(this IServiceCollection services)
+    {
+        services.AddOcpiExceptionHandler();
+        services.AddOcpiVersioning();
+        return services;
     }
 }
---
+++ src/OCPI.Net/Versioning/AddOcpiVersioningExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OCPI.Versioning;
+
+public static class AddOcpiVersioningExtension
+{
+    public static WebApplicationBuilder AddOcpiVersioning(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddOcpiVersioning();
+        return builder;
+    }
+
+    public static IServiceCollection AddOcpiVersioning(this IServiceCollection services)
+    {
+        return services;
+    }
+}
