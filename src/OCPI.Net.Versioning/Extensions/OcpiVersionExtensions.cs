d40ead382b3ca1bf1b1e407dca412d4f13ca92ed
Yuriy Durov <u.a.durov@gmail.com>
2023-06-14 22:33:59 +0400

    Add OcpiVersionExtensions

---
+++ src/OCPI.Net.Versioning/Extensions/OcpiVersionExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace OCPI;
+
+public static class OcpiVersionExtensions
+{
+    public static bool RequiresTokenEncoding(this OcpiVersion version)
+    {
+        return version >= OcpiVersion.v2_2;
+    }
+}
