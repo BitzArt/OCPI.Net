d6e39207f95ec452a45e8bed9639fe0347f8623f
Lior Ben Ari <liorba88@gmail.com>
2023-09-19 10:10:13 +0300

    OcpiLocation object - 2.1.1 properties (validation missing) (#11)
    * Location object - add 2.1.1 property - LocationType
    
    * OCPI 2.1.1 properties on OcpiCOnnector object
    
    ---------
    
    Co-authored-by: Lior Ben Ari <liorb@nayax.com>

--- src/OCPI.Net.Contracts/Location/OcpiConnector.cs
+++ src/OCPI.Net.Contracts/Location/OcpiConnector.cs
@@ -16,14 +16,26 @@ public class OcpiConnector
     [JsonPropertyName("power_type")]
     public PowerType? PowerType { get; set; }
 
+    //OCPI 2.1.1 property
+    [JsonPropertyName("voltage")]
+    public int? Voltage { get; set; }
+
     [JsonPropertyName("max_voltage")]
     public int? MaxVoltage { get; set; }
+
+    //OCPI 2.1.1 property
+    [JsonPropertyName("amperage")]
+    public int? Amperage { get; set; }
 
     [JsonPropertyName("max_amperage")]
     public int? MaxAmperage { get; set; }
 
     [JsonPropertyName("max_electric_power")]
     public int? MaxElectricPower { get; set; }
+
+    //OCPI 2.1.1 property
+    [JsonPropertyName("tariff_id")]
+    public string? TariffId { get; set; }
 
     [JsonPropertyName("tariff_ids")]
     public IEnumerable<string>? TariffIds { get; set; }
---
+++ src/OCPI.Net.Core/Enums/Location/LocationType.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.Serialization;
+
+namespace OCPI;
+
+public enum LocationType : byte
+{
+    /// <summary>
+    /// Parking in public space.
+    /// </summary>
+    [EnumMember(Value = "UNKNOWN")]
+    Unknown = 0,
+
+    //======================================
+
+    /// <summary>
+    /// Parking in public space.
+    /// </summary>
+    [EnumMember(Value = "ON_STREET")]
+    OnStreet = 11,
+
+    //======================================
+
+    /// <summary>
+    /// Multistorey car park
+    /// </summary>
+    [EnumMember(Value = "PARKING_GARAGE")]
+    ParkingGarage = 21,
+
+    /// <summary>
+    /// A cleared area that is intended for parking vehicles,
+    /// i.e. at super markets, bars, etc.
+    /// </summary>
+    [EnumMember(Value = "PARKING_LOT")]
+    ParkingLot = 22,
+
+    /// <summary>
+    /// Multistorey car park, mainly underground.
+    /// </summary>
+    [EnumMember(Value = "UNDERGROUND_GARAGE")]
+    UndergroundGarage = 23,
+
+    //======================================
+
+    /// <summary>
+    /// Parking in public space.
+    /// </summary>
+    [EnumMember(Value = "OTHER")]
+    Other = 255,
+}
