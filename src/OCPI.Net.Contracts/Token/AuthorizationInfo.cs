d5cd0d89f8b4a2de66c7435bc6119953a819ca37
Lior Ben Ari <liorb@nayax.com>
2023-12-25 11:16:11 +0200

    Objects for Tokens authorization logic

---
+++ src/OCPI.Net.Contracts/Token/AuthorizationInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OCPI.Contracts;
+
+public class AuthorizationInfo
+{
+    /// <summary>
+    /// Status of the Token, and whether charging is allowed at the optionally
+    /// given location.
+    /// </summary>
+    [JsonPropertyName("allowed")]
+    public AllowedType Allowed { get; set; }
+
+    /// <summary>
+    /// The complete Token object for which this authorization was requested.
+    /// </summary>
+    [JsonPropertyName("token")]
+    public OcpiToken Token { get; set; }
+
+    /// <summary>
+    /// Optional reference to the location if it was included in the request, and if
+    /// the EV driver is allowed to charge at that location.Only the EVSEs the
+    /// EV driver is allowed to charge at are returned.
+    /// </summary>
+    [JsonPropertyName("location")]
+    public LocationReferences? LocationReferences { get; set; }
+
+    /// <summary>
+    /// Reference to the authorization given by the eMSP, when given, this
+    /// reference will be provided in the relevant Session and/or CDR.
+    /// </summary>
+    [JsonPropertyName("authorization_reference")]
+    public string? AuthorizationReference { get; set; }
+
+    /// <summary>
+    /// Optional display text, additional information to the EV driver.
+    /// </summary>
+    [JsonPropertyName("info")]
+    public OcpiDisplayText? Info { get; set; }
+
+}
---
+++ src/OCPI.Net.Contracts/Token/LocationReferences.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OCPI.Contracts;
+
+public class LocationReferences
+{
+    /// <summary>
+    /// Unique identifier for the location.
+    /// </summary>
+    [JsonPropertyName("location_id")]
+    public string LocationId { get; set; }
+
+    /// <summary>
+    /// Unique identifiers for EVSEs within the CPO’s platform for the EVSE within the
+    /// given location.
+    /// </summary>
+    [JsonPropertyName("evse_uids")]
+    public string? EvseUids { get; set; }
+
+    //==================== Deprecated in OCPI 2.2 ========================
+
+    /// <summary>
+    /// Identifies the connectors within the given EVSEs.
+    /// </summary>
+    [OcpiDeprecated(after: "2.1.1")]
+    [JsonPropertyName("connector_ids")]
+    public string? ConnectorIds { get; set; }
+
+}
