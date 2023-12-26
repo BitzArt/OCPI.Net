b77dd4331e8a65a03ecd19962922827a961e72a1
Lior Ben Ari <liorba88@gmail.com>
2023-12-26 12:00:37 +0200

    Objects for Token authorization logic (#23)
    * Objects for Tokens authorization logic
    
    * add new lines at end of files
    
    ---------
    
    Co-authored-by: Lior Ben Ari <liorb@nayax.com>

---
+++ src/OCPI.Net.Core/Enums/Token/AllowedType.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Serialization;
+
+namespace OCPI;
+
+public enum AllowedType : byte
+{
+    //====================1x: Allowed========================
+    /// <summary>
+    /// This Token is allowed to charge (at this location).
+    /// </summary>
+    [EnumMember(Value = "ALLOWED")]
+    Allowed = 11,
+
+    //====================2x: Not Allowed========================
+
+    /// <summary>
+    /// This Token is blocked.
+    /// </summary>
+    [EnumMember(Value = "BLOCKED")]
+    Blocked = 21,
+
+    /// <summary>
+    /// This Token has expired.
+    /// </summary>
+    [EnumMember(Value = "EXPIRED")]
+    Expired = 22,
+
+    /// <summary>
+    /// This Token belongs to an account that has not enough credits to charge (at the given location).
+    /// </summary>
+    [EnumMember(Value = "NO_CREDIT")]
+    NoCredit = 23,
+
+    /// <summary>
+    /// Token is valid, but is not allowed to charge at the given location.
+    /// </summary>
+    [EnumMember(Value = "NOT_ALLOWED")]
+    NotAllowed = 24
+}
+
